c0bf48c965de0ec0642a72a28c0f8be56f1ae7da
NehanPathan <pathannehan26@gmail.com>
2024-02-29 23:58:03 +0530

    Add Action For Reducer In NgRx

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,12 +1,14 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from '@ngrx/store';
+import { increment } from './counter.actions';
 
 const initialState = 0;
 
-// export const counterReducer = createReducer(
-//     initialState
-// );
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1)
+);
 
 //alternative way to create reducer
-export function counterReducer(state = initialState){
-    return initialState;
-}
+// export function counterReducer(state = initialState){
+//     return initialState;
+// }
