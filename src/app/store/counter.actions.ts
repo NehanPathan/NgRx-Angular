c35b5cd35ea215f8b5e6bdcdc2b09469a519377d
NehanPathan <pathannehan26@gmail.com>
2024-03-01 22:24:04 +0530

    Add Actions And State Changing Reducer Manually And Comment It Out And Also Add This For Increment And Decrement

--- src/app/store/counter.actions.ts
+++ src/app/store/counter.actions.ts
@@ -1,5 +1,23 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props, Action } from '@ngrx/store';
 
 export const increment = createAction(
-    '[Counter] Increment'
+  '[Counter] Increment',
+  props<{ value: number }>()
+);
+
+// export const INCREMENT = '[Counter] Increment';
+
+//alternative way to create action
+
+// export class IncrementAction implements Action {
+//   readonly type = INCREMENT;
+
+//   constructor(public value: number) {}
+// }
+
+// export type CounterActions = IncrementAction;
+
+export const decrement = createAction(
+  '[Counter] Decrement',
+  props<{ value: number }>()
 );
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,14 +1,22 @@
-import { createReducer, on } from '@ngrx/store';
-import { increment } from './counter.actions';
+import { Action, createReducer, on } from '@ngrx/store';
+// import { CounterActions, INCREMENT, IncrementAction } from './counter.actions';
+import { decrement, increment } from './counter.actions';
 
 const initialState = 0;
 
 export const counterReducer = createReducer(
   initialState,
-  on(increment, (state) => state + 1)
+  on(increment, (state, action) => state + action.value),
+  on(decrement, (state, action) => state - action.value)
 );
 
 //alternative way to create reducer
-// export function counterReducer(state = initialState){
-//     return initialState;
+// export function counterReducer(
+//   state = initialState,
+//   action: CounterActions | Action
+// ) {
+//   if (action.type === INCREMENT) {
+//     return state + (action as IncrementAction).value;
+//   }
+//   return initialState;
 // }
